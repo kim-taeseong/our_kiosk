11706027cddff692f375269fb15b2579ee0b28a3
taeseong <dnfrkwhr2424@naver.com>
2024-04-03 17:31:06 +0900

    feat: 모델 Variety, Beverage 생성

--- our_kiosk_app/models.py
+++ our_kiosk_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Variety(models.Model):
+    # 패스오더 앱 -> 최대 10자(공백포함)
+    name = models.CharField(max_length=15)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+class Beverage(models.Model):
+    variety = models.ForeignKey(Variety, on_delete=models.CASCADE)
+    # 패스오더 앱 -> 최대 18자(공백포함)
+    name = models.CharField(max_length=25)
+    registration_date = models.DateTimeField(auto_now_add=True)
