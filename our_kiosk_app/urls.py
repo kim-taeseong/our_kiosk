5ecc7af3018a8b5a21e89d4cd05beeb5abfc318e
taeseong <dnfrkwhr2424@naver.com>
2024-04-08 15:16:45 +0900

    feat: variety를 추가하는 기능 추가

--- our_kiosk_app/urls.py
+++ our_kiosk_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'our_kiosk_app'
 
 urlpatterns = [
-    path('', views.VarietyListView.as_view(), name='index')
+    path('', views.VarietyListView.as_view(), name='index'),
+    path('variety/create/', views.VarietyCreateView.as_view(), name='create_variety'),
 ]
--- our_kiosk_app/views.py
+++ our_kiosk_app/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 from .models import Variety, Beverage
 
 class VarietyListView(ListView):
     model = Variety
+
+class VarietyCreateView(CreateView):
+    model = Variety
+    fields = ['name']
+    success_url = reverse_lazy('our_kiosk_app:index')
