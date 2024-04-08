5ec775f4ca725de238bc1d11050513d6dff19b33
taeseong <dnfrkwhr2424@naver.com>
2024-04-08 15:48:23 +0900

    feat: Beverage 추가

--- our_kiosk_app/admin.py
+++ our_kiosk_app/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Variety
+from .models import Variety, Beverage
 
 @admin.register(Variety)
 class VarietyAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'registration_date')
+    search_fields = ('name',)
+
+@admin.register(Beverage)
+class Beverage(admin.ModelAdmin):
+    list_display = ('id', 'name', 'registration_date', 'variety')
     search_fields = ('name',)
