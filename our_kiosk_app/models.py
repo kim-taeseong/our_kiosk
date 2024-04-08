8a5b24a727b5d42699c40b03bb09868496a04ae6
taeseong <dnfrkwhr2424@naver.com>
2024-04-08 15:45:46 +0900

    feat: Variety, Beverage에 __str__함수 추가

--- our_kiosk_app/models.py
+++ our_kiosk_app/models.py
@@ -5,8 +5,14 @@ class Variety(models.Model):
     name = models.CharField(max_length=15)
     registration_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 class Beverage(models.Model):
     variety = models.ForeignKey(Variety, on_delete=models.CASCADE)
     # 패스오더 앱 -> 최대 18자(공백포함)
     name = models.CharField(max_length=25)
     registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
